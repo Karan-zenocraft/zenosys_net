e05147ba30d7246fac7235fe10648fbc87da0bea
root <root@localhost.creatusello.com>
2019-11-01 18:10:35 +0530

    industries and images and icons

--- application/views/industries/education-industry.php
+++ application/views/industries/education-industry.php
@@ -1,45 +1,88 @@
         <div class="container">
-            <div class="row WhyZenosysInner d-flex align-items-center align-items-center">
-
-                <div class="col-md-3 WhyZenosysInnerBox">
-                    <img src="<?php echo base_url(); ?>assets/images/icon1.png" alt="" class="img-fluid">
-                    <h6 class="TitleH6">RESPONSIBILITY</h6>
+             <div class="slick-carousel row WhyZenosysInner d-flex align-items-center align-items-center">
+                <div>
+                    <div class="slide-content col-md-12 WhyZenosysInnerBox">
+                    <img src="<?php echo base_url(); ?>assets/images/finance_icon.png" alt="" class="img-fluid">
+                    <h6 class="TitleH6">Financial Services</h6>
                     <p>Equipped with industry's leading page
                         builder & admin panel. With lifetime
                         support for any questions</p>
 
                 </div>
-                <div class="col-md-3 WhyZenosysInnerBox">
-                    <img src="<?php echo base_url(); ?>assets/images/icon2.png" alt="" class="img-fluid">
+                </div>
+                <div>
+                    <div class="slide-content col-md-12 WhyZenosysInnerBox">
 
-                    <h6 class="TitleH6">COMMITMENT</h6>
+                    <img src="<?php echo base_url(); ?>assets/images/government_icon.png" alt="" class="img-fluid">
+
+                    <h6 class="TitleH6">Government Bodies
                     <p>Bunch of styles, features, elements &
                         samples give you an absolute design
                         freedom in creating webpages</p>
                 </div>
-                <div class="col-md-3 WhyZenosysInnerBox">
-                    <img src="<?php echo base_url(); ?>assets/images/icon3.png" alt="" class="img-fluid">
+                </div>
+                <div>
+                    <div class="slide-content col-md-12 WhyZenosysInnerBox">
+                    <img src="<?php echo base_url(); ?>assets/images/education_icon.png" alt="" class="img-fluid">
 
-                    <h6 class="TitleH6">TEAM WORK</h6>
+                    <h6 class="TitleH6">Education Industry</h6>
                     <p>With Scalia you will make beautiful
                         pixel perfect pages, with every element
                         precisely positioned</p>
 
                 </div>
-                <div class="col-md-3 WhyZenosysInnerBox">
-                    <img src="<?php echo base_url(); ?>assets/images/icon4.png" alt="" class="img-fluid">
+                </div>
+                <div>
+                    <div class="slide-content col-md-12 WhyZenosysInnerBox">
+                    <img src="<?php echo base_url(); ?>assets/images/retail_icon.png" alt="" class="img-fluid">
 
-                    <h6 class="TitleH6">INNOVATION</h6>
+                    <h6 class="TitleH6">Retail, Distribution and E-Commerce</h6>
                     <p>Scalia is future-proof best-of-breed
                         premium theme with unlimited styling
                         customization options</p>
 
                 </div>
+                </div>
+                    <div>
+                    <div class="slide-content col-md-12 WhyZenosysInnerBox">
+                    <img src="<?php echo base_url(); ?>assets/images/telecom_icon.png" alt="" class="img-fluid">
+
+                    <h6 class="TitleH6">Telecommunication</h6>
+                    <p>Scalia is future-proof best-of-breed
+                        premium theme with unlimited styling
+                        customization options</p>
+
+                </div>
+                </div>
+                    <div>
+                    <div class="slide-content col-md-12 WhyZenosysInnerBox">
+                    <img src="<?php echo base_url(); ?>assets/images/helthcare_icon.png" alt="" class="img-fluid">
+
+                    <h6 class="TitleH6">Healthcare Industry</h6>
+                    <p>Scalia is future-proof best-of-breed
+                        premium theme with unlimited styling
+                        customization options</p>
+
+                </div>
+                </div>
+                   <div>
+                    <div class="slide-content col-md-12 WhyZenosysInnerBox">
+                    <img src="<?php echo base_url(); ?>assets/images/menufacturing_icon.png" alt="" class="img-fluid">
+
+                    <h6 class="TitleH6">Manufacturing Industry</h6>
+                    <p>Scalia is future-proof best-of-breed
+                        premium theme with unlimited styling
+                        customization options</p>
+
+                </div>
+                </div>
+
 
             </div>
 
 
             <div class="row">
+
                 <div class="col-md-8 WhyZenosysContent">
 
                     <h2 class="TitleH2">Education Industry</h2>
@@ -52,7 +95,7 @@
 
                 </div>
                  <div class="col-md-4 WhyZenosysContent">
-                    <img src="<?php echo base_url(); ?>assets/images/why_zenosys.png" width="100%" height="100%">
+                     <img src="<?php echo base_url(); ?>assets/images/education.png" class="img-fluid CommanImg">
                  </div>
             </div>
 
