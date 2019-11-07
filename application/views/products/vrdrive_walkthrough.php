34847285743f38951c76dfeb6381d6a447865d4d
root <root@localhost.creatusello.com>
2019-11-07 17:12:57 +0530

    products icons and oracle icons changes

--- application/views/products/vrdrive_walkthrough.php
+++ application/views/products/vrdrive_walkthrough.php
@@ -1,55 +1,55 @@
         <div class="container">
-       <div class="slick-carousel row WhyZenosysInner d-flex align-items-center align-items-center">
+    <div class="slick-carousel row WhyZenosysInner d-flex align-items-center align-items-center">
+
                 <div><div class="slide-content col-md-12 WhyZenosysInnerBox">
-                    <img src="<?php echo base_url(); ?>assets/images/icon4.png" alt="" class="img-fluid">
+                    <img src="<?php echo base_url(); ?>assets/images/VRDRIVE-WALKTHROUGH-icon.png" alt="" class="img-fluid">
 
                     <h6 class="TitleH6">VRDRIVE WALKTHROUGH</h6>
                     <p>Web based visualizations for translating ideas</p>
 
                 </div></div>
-
                 <div><div class="slide-content col-md-12 WhyZenosysInnerBox">
-                    <img src="<?php echo base_url(); ?>assets/images/icon1.png" alt="" class="img-fluid">
+                    <img src="<?php echo base_url(); ?>assets/images/ALWAYS-AHEAD---SUPPLY-CHAIN-icon.png" alt="" class="img-fluid">
                     <h6 class="TitleH6">ALWAYS AHEAD - SUPPLY CHAIN</h6>
                     <p>The power of Machine Learning for improving performance and minimizing risks</p>
 
                 </div></div>
                 <div><div class="slide-content col-md-12 WhyZenosysInnerBox">
-                    <img src="<?php echo base_url(); ?>assets/images/icon2.png" alt="" class="img-fluid">
+                    <img src="<?php echo base_url(); ?>assets/images/CHURNPREDICT---K-FACTOR-con.png" alt="" class="img-fluid">
 
                     <h6 class="TitleH6">CHURNPREDICT - K FACTOR</h6>
                     <p>Versatile solution for better understanding your customers</p>
                 </div></div>
                 <div><div class="slide-content col-md-12 WhyZenosysInnerBox">
-                    <img src="<?php echo base_url(); ?>assets/images/icon3.png" alt="" class="img-fluid">
+                    <img src="<?php echo base_url(); ?>assets/images/MYVIEW---SECURITY-icon.png" alt="" class="img-fluid">
 
                     <h6 class="TitleH6">MYVIEW - SECURITY</h6>
                     <p>A cloud based solution for smart security and monitoring needs</p>
 
                 </div></div>
                         <div><div class="slide-content col-md-12 WhyZenosysInnerBox">
-                    <img src="<?php echo base_url(); ?>assets/images/icon4.png" alt="" class="img-fluid">
+                    <img src="<?php echo base_url(); ?>assets/images/DEGREE360-HRMS-icon.png" alt="" class="img-fluid">
 
                     <h6 class="TitleH6">DEGREE360 HRMS</h6>
                     <p>A smart and unique cloud-based system for Human Resource Management</p>
 
                 </div></div>
                         <div><div class="slide-content col-md-12 WhyZenosysInnerBox">
-                    <img src="<?php echo base_url(); ?>assets/images/icon4.png" alt="" class="img-fluid">
+                    <img src="<?php echo base_url(); ?>assets/images/JAKARU-POSTGRES-BDR-icon.png" alt="" class="img-fluid">
 
                     <h6 class="TitleH6">JAKARU POSTGRES BDR</h6>
                     <p>A highly efficient bi-directional replication system dedicated towards Postgres Databases</p>
 
                 </div></div>
                         <div><div class="slide-content col-md-12 WhyZenosysInnerBox">
-                    <img src="<?php echo base_url(); ?>assets/images/icon4.png" alt="" class="img-fluid">
+                    <img src="<?php echo base_url(); ?>assets/images/POLYGLOT-DBMONITOR-icon.png" alt="" class="img-fluid">
 
                     <h6 class="TitleH6">POLYGLOT DBMONITOR</h6>
                     <p>A unique mechanism for managing database technologies</p>
 
                 </div></div>
                         <div><div class="slide-content col-md-12 WhyZenosysInnerBox">
-                    <img src="<?php echo base_url(); ?>assets/images/icon4.png" alt="" class="img-fluid">
+                    <img src="<?php echo base_url(); ?>assets/images/WAREHOUSE-CONTROLLER-icon.png" alt="" class="img-fluid">
 
                     <h6 class="TitleH6">WAREHOUSE CONTROLLER</h6>
                     <p>A customized warehouse management system for a hassle-free experience</p>
