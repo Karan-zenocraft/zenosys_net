1fd0d5db75ace8f8c46445b7fab34bbcd59f0209
Karan Solanki <KSolanki@ZENOSYS.LOCAL>
2019-11-18 19:35:03 +0530

    sap tabbing

--- application/views/enterprise-applications/sap.php
+++ application/views/enterprise-applications/sap.php
@@ -1,4 +1,64 @@
-        <div class="container">
+<style>
+.accordion {
+  background-color: #eee;
+  color: #464852;
+  cursor: pointer;
+  padding: 15px;
+display: block;
+  width: 100%;
+  border: none;
+  text-align: left;
+  outline: none;
+  font-size: 13px;
+  transition: 0.4s;
+    letter-spacing: 1px;
+    line-height: 20px;
+    font-family: 'Arial';
+    font-weight: 300;
+    text-transform: uppercase;
+}
+    .panel p{margin: 10px 0px;color: #6F7072;letter-spacing: 0.8px;line-height: 24px;padding: 5px;}
+.active, .accordion:hover {
+  background-color: #ccc;
+}
+
+.accordion:after {
+  content: '\002B';
+  color: #777;
+  font-weight: bold;
+  float: right;
+  margin-left: 5px;
+}
+
+.active:after {
+  content: "\2212";
+}
+
+.panel {
+  padding: 0 0px;
+  background-color: white;
+  max-height: 0;
+  overflow: hidden;
+  transition: max-height 0.2s ease-out;
+}
+    .accordionColumn{margin-bottom: 30px;}
+    
+    @media (min-width: 320px) and (max-width: 767px) {
+        .accordionColumn:first-child{margin-top: 30px;margin-bottom: 0px;}
+        .accordion{padding: 10px;font-size: 11px;}
+        .panel p{font-size: 12px;padding: 5px;}
+}
+    
+    @media (min-width: 768px) and (max-width: 1200px) {
+        
+        .accordion{padding: 10px;font-size: 11px;}
+        .panel p{font-size: 12px;padding: 5px;}
+}
+    
+</style>       
+
+
+<div class="container">
  <div class="slick-carousel row WhyZenosysInner d-flex align-items-center align-items-center">
                 <div>
                     <div class="slide-content col-md-12 WhyZenosysInnerBox">
@@ -58,6 +118,110 @@
             <img src="<?php echo base_url(); ?>assets/images/sap.png" class="img-fluid CommanImg GovernmentImg fullwidth fullwidth2 Retail Telecommunication" style="bottom:0;top:0;margin:auto;">
         </div>
             </div>
+    
+    
+    <div class="row CustomeRows">
+    
+    <div class="col-md-6 accordionColumn">
+<a class="accordion">SAP S/4 HANA Implementation</a>
+<div class="panel">
+  <p>Successful SAP S/4HANA implementation is based on a lot of key factors that play an integral role in getting the best out of it, including API led design, SAP custom code, thorough know-how of platforms such as Eclipse and Github as well as relevant testing tools. 
+</p>
+</div>
+
+<a class="accordion">S/4 HANA Migration</a>
+<div class="panel">
+  <p>Transforming existing systems to SAP S/4 HANA is a task, which when done right, offers seamless productivity and efficiency. But this can often be challenging. Whether its implementing a new system in place of existing or converting an existing system or a complete landscape transformation, the deeply experienced team at Zenosys offers the perfect solution.
+</p>
+</div>
+
+<a class="accordion">Enhancement & Optimization </a>
+<div class="panel">
+  <p>There’s only so much you can get out of SAP S/4 HANA. But you can make the most when the tools are enhanced and optimized to fit your need and work best in accordance with it. From unified transactions and analytic data store to integrated computation engine for planning and forecasting, with a range of other optimizations in between, Zenosys helps you drive the best value out of SAP S/4 HANA for your business goal.
+</p>
+</div>
+        
+        
+        <a class="accordion">SAP Rollout Services</a>
+<div class="panel">
+  <p>With the perfect and proven practices in places, Zenosys offers you seamless SAP rollout services, ensuring a simplified and effortless launch for you system. From template rollout to country extensions and custom development, successful deployment along with training, development and support, we help make the process a cake-walk. 
+
+</p>
+</div>
+        
+        
+        <a class="accordion">SAP Support</a>
+<div class="panel">
+  <p>The expert SAP support team at Zenosys handholds numerous clients globally and ensures that their systems are performing at their best. Through a range of systems including embedded and dedicated teams, remote technology support and long to short term support plans, we help our clients with successful implementation, innovation in strategies, seamless support and much more.
+ 
+
+</p>
+</div>
+  </div>  
+        
+        
+        
+        
+        <div class="col-md-6 accordionColumn">
+<a class="accordion">Cloud Analytics</a>
+<div class="panel">
+  <p>
+It is truly, the future of efficient decision making. SAP Cloud Analytics helps you in making end-to-end decision making in all the important functions of your business, empowering you with the data to plan and perform better. Zenosys helps you implement the system with complete finesse and makes sure that you get the best value out it.
+
+</p>
+</div>
+
+<a class="accordion">C4C
+</a>
+<div class="panel">
+  <p>SAP C4C, also known as Cloud for Customer, is a versatile solution that helps you mange sales, service as well as marketing activities in order to enhance your customer relationship management skills. Zenosys hails with great expertise as well as experience with SAP Cloud for Sales, Marketing and Social Engagement, to provide you with the best solution. 
+
+</p>
+</div>
+
+<a class="accordion">Success Factor </a>
+<div class="panel">
+  <p>SAP Success Factor is an industry-leading, cloud based Human Capital Management system, that adds heart to your HR system and transforms it to provide an unparalleled experience while enhancing all functionalitites. Zenosys excels at helping companies adopt and implement the power of Success Factor into their organizational structure with the right solutions. 
+</p>
+</div>
+        
+        
+        <a class="accordion">Audit Support
+</a>
+<div class="panel">
+  <p>A one-stop solution for your auditing needs- SAP Audit Management, that helps you streamline the complex auditing process and improve the quality of your auditing manifolds. Zenosys provides seamless support for your auditing needs and helps you with end to end services including on-premise as well as cloud deployment, integration with different SAP tools and thorough hand-holding.
+
+</p>
+</div>
+        
+        
+        <a class="accordion">Barcode/RFID
+</a>
+<div class="panel">
+  <p>Zenosys also specializes in providing RFID (Radio Frequency Identification) and Barcode integration with SAP. Through these tools we can help you drastically improve productivity and efficiency, including services like setting up core services, integrating and end to end support.  
+
+ 
+
+</p>
+</div>
+  </div>  
+    </div>
 
 
         </div>
+<script>
+var acc = document.getElementsByClassName("accordion");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var panel = this.nextElementSibling;
+    if (panel.style.maxHeight) {
+      panel.style.maxHeight = null;
+    } else {
+      panel.style.maxHeight = panel.scrollHeight + "px";
+    } 
+  });
+}
+</script>
