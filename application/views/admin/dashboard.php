85ea0721f635887162b2f92b43d739288ae6c647
Karan Solanki <KSolanki@ZENOSYS.LOCAL>
2019-12-13 13:38:47 +0530

    css dashboard

--- application/views/admin/dashboard.php
+++ application/views/admin/dashboard.php
@@ -3,14 +3,14 @@
 
 <div class="row">
             <a href="<?php echo base_url() ?>admin/Manage_Users" class="Ablock"><div class="col-xl-4 col-sm-6 mb-3">
-              <div class="card text-white bg-primary o-hidden h-100">
+              <div class="card text-black bg-primary o-hidden h-100">
                 <div class="card-body">
                   <div class="card-body-icon">
                    <i class="fas fa-user-tie"></i>
                   </div>
                   <div class="mr-5"><?php echo htmlentities($tcount); ?> Users</div>
                 </div>
-                <a class="card-footer text-white clearfix small z-1">
+                <a class="card-footer text-black clearfix small z-1 Ablock" href="<?php echo base_url() ?>admin/Manage_Users">
                   <span class="float-left">Total Registered Users</span>
                   <span class="float-right">
                     <i class="fas fa-angle-right"></i>
@@ -20,14 +20,14 @@
             </div>
           </a>
             <a href="<?php echo base_url() ?>admin/Careers" class="Ablock"><div class="col-xl-4 col-sm-6 mb-3">
-              <div class="card text-white bg-warning o-hidden h-100">
+              <div class="card text-black bg-warning o-hidden h-100">
                 <div class="card-body">
                   <div class="card-body-icon">
-                    <i class="fas fa-list"></i>
+                    <i class="fas fa-fw fa-graduation-cap"></i>
                   </div>
                   <div class="mr-5"><?php echo htmlentities($tsevencount); ?> Careers</div>
                 </div>
-                <a class="card-footer text-white clearfix small z-1" href="#">
+                <a class="card-footer text-black clearfix small z-1 Ablock" href="<?php echo base_url() ?>admin/Careers">
                   <span class="float-left">Added in Last 10 Days</span>
                   <span class="float-right">
                     <i class="fas fa-angle-right"></i>
@@ -36,14 +36,14 @@
               </div>
             </div></a>
     <!--         <div class="col-xl-4 col-sm-6 mb-3">
-              <div class="card text-white bg-success o-hidden h-100">
+              <div class="card text-black bg-success o-hidden h-100">
                 <div class="card-body">
                   <div class="card-body-icon">
                    <i class="fas  fa-list-alt"></i>
                   </div>
                   <div class="mr-5"><?php echo htmlentities($tthirycount); ?> Category</div>
                 </div>
-                <a class="card-footer text-white clearfix small z-1" href="#">
+                <a class="card-footer text-black clearfix small z-1 Ablock" href="#">
                   <span class="float-left">Registered in last 30 days</span>
                   <span class="float-right">
                     <i class="fas fa-angle-right"></i>
