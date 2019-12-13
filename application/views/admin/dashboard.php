a14053bb04ad45d3fb072206323a5899a2d649f2
root <root@localhost.creatusello.com>
2019-12-13 12:49:08 +0530

    links added in admin dashboard

--- application/views/admin/dashboard.php
+++ application/views/admin/dashboard.php
@@ -2,7 +2,7 @@
 
 
 <div class="row">
-            <div class="col-xl-4 col-sm-6 mb-3">
+            <a href="<?php echo base_url() ?>admin/Manage_Users"><div class="col-xl-4 col-sm-6 mb-3">
               <div class="card text-white bg-primary o-hidden h-100">
                 <div class="card-body">
                   <div class="card-body-icon">
@@ -18,23 +18,24 @@
                 </a>
               </div>
             </div>
-            <div class="col-xl-4 col-sm-6 mb-3">
+          </a>
+            <a href="<?php echo base_url() ?>admin/Careers"><div class="col-xl-4 col-sm-6 mb-3">
               <div class="card text-white bg-warning o-hidden h-100">
                 <div class="card-body">
                   <div class="card-body-icon">
                     <i class="fas fa-list"></i>
                   </div>
-                  <div class="mr-5"><?php echo htmlentities($tsevencount); ?> Portfolio</div>
+                  <div class="mr-5"><?php echo htmlentities($tsevencount); ?> Careers</div>
                 </div>
                 <a class="card-footer text-white clearfix small z-1" href="#">
-                  <span class="float-left">Registred in Last 10 Days</span>
+                  <span class="float-left">Added in Last 10 Days</span>
                   <span class="float-right">
                     <i class="fas fa-angle-right"></i>
                   </span>
                 </a>
               </div>
-            </div>
-            <div class="col-xl-4 col-sm-6 mb-3">
+            </div></a>
+    <!--         <div class="col-xl-4 col-sm-6 mb-3">
               <div class="card text-white bg-success o-hidden h-100">
                 <div class="card-body">
                   <div class="card-body-icon">
@@ -49,6 +50,6 @@
                   </span>
                 </a>
               </div>
-            </div>
+            </div> -->
 
           </div>
