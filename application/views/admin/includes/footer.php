2b4a387bbc83ad3f0ae0611fb5e7ab3ffa1ed90d
root <root@localhost.creatusello.com>
2019-12-13 13:07:56 +0530

    add copyright year

--- application/views/admin/includes/footer.php
+++ application/views/admin/includes/footer.php
@@ -3,7 +3,7 @@
 <footer class="sticky-footer">
           <div class="container my-auto">
             <div class="copyright text-center my-auto">
-              <b>Copyright © <a href="http://zenosys.com" target="_blank">Zenosys</a> 2019</b>
+              <b>Copyright © <a href="http://zenosys.com" target="_blank">Zenosys </a><?php echo date("Y"); ?></b>
             </div>
           </div>
         </footer>
