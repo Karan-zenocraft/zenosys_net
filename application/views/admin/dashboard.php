55f3e50790a84b5466dacb9e5878c62efe0666dc
Karan Solanki <KSolanki@ZENOSYS.LOCAL>
2019-12-13 12:58:21 +0530

    a tag

--- application/views/admin/dashboard.php
+++ application/views/admin/dashboard.php
@@ -2,7 +2,7 @@
 
 
 <div class="row">
-            <a href="<?php echo base_url() ?>admin/Manage_Users"><div class="col-xl-4 col-sm-6 mb-3">
+            <a href="<?php echo base_url() ?>admin/Manage_Users" class="Ablock"><div class="col-xl-4 col-sm-6 mb-3">
               <div class="card text-white bg-primary o-hidden h-100">
                 <div class="card-body">
                   <div class="card-body-icon">
@@ -19,7 +19,7 @@
               </div>
             </div>
           </a>
-            <a href="<?php echo base_url() ?>admin/Careers"><div class="col-xl-4 col-sm-6 mb-3">
+            <a href="<?php echo base_url() ?>admin/Careers" class="Ablock"><div class="col-xl-4 col-sm-6 mb-3">
               <div class="card text-white bg-warning o-hidden h-100">
                 <div class="card-body">
                   <div class="card-body-icon">
